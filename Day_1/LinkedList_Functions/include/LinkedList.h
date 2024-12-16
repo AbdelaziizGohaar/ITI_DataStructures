6736c98e687e28b7523eb09d60858fa1700990f8
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2024-12-17 00:10:57 +0200

    My first Project in DataStructure

---
+++ Day_1/LinkedList_Functions/include/LinkedList.h
@@ -0,0 +1,32 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "Node.h"
+
+class LinkedList
+{
+    public:
+    Node * head ;
+    Node * tail ;
+
+        LinkedList()
+         {
+           head = NULL;
+           tail = NULL;
+         }
+
+        ~LinkedList() {}
+
+        void add(int data)
+        {
+           if (head == NULL)
+           {
+
+           }
+        }
+
+//    protected:
+
+//    private:
+};
+
+#endif // LINKEDLIST_H
---
+++ Day_1/LinkedList_Functions/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "LinkedList.h"
+
+using namespace std;
+
+int main()
+{
+
+ LinkedList azizLink ;
+
+
+    return 0;
+}
