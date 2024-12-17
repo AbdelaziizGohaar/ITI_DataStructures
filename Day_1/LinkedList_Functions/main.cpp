7c7d68289c9ad3edae7763c5d9be4a3b289798bb
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2024-12-17 02:48:41 +0200

    update of my code for linkedlist Functions

--- Day_1/LinkedList_Functions/main.cpp
+++ Day_1/LinkedList_Functions/main.cpp
@@ -13,6 +13,7 @@ azizLl.add(9);
 
 azizLl.InsertAfter(100,7);
 //azizLl.InsertBefore(900,6);
+cout<<"data of index one : "<<azizLl.GetDataByIndex(1)<<endl;
 cout<<"element count is "<<azizLl.GetCount()<<endl;
 cout<<"element in linkedlist is : "<<endl;
 azizLl.display();
